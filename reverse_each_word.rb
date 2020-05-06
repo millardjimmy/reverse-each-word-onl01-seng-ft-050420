0c77a5362e0ac0d8f3a62861e94daea3918fe379
Jimmy Millard <millardjimmy@gmail.com>
2020-05-06 19:32:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   backword_array = sentence.split(" ")
   anew_array - []
-  anew_array = backword_array.collect{}
+  anew_array = backword_array.collect{|word| word.reverse}
+  anew_array.join (" ")
 end
